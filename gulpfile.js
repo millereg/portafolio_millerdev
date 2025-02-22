13f2e78f5f86a1e2157fdd727d64b65005e327ff
millereg <gemillert4090@outlook.com>
2025-02-22 17:50:42 -0500

    Configuracion Inicial del Proyecto

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+"use strict";
+
+const { src, dest, watch, series } = require("gulp");
+
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const cssnano = require("cssnano");
+
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
+
+const uglify = require("gulp-uglify");
+
+const htmlmin = require("gulp-htmlmin");
+const replace = require("gulp-replace");
+
+const newer = require("gulp-newer");
+
+function css(done) {
+  src("src/scss/app.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(replace("../../../../assets/", "../img/"))
+    .pipe(replace(/\.(png|jpg)/g, ".webp"))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("docs/css"));
+
+  done();
+}
+
+function javascript() {
+  return src("src/js/**/*.js").pipe(uglify()).pipe(dest("docs/js"));
+}
+
+function imagenes() {
+  return src("assets/**/*", { encoding: false })
+    .pipe(newer("docs/img"))
+    .pipe(imagemin({ optimizationLevel: 3 }))
+    .pipe(dest("docs/img", { encoding: false }));
+}
+
+function html() {
+  return src("index.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(replace("docs/", ""))
+    .pipe(replace(/assets\/(.*?\.svg)/g, "$1"))
+    .pipe(replace(/src\//g, ""))
+    .pipe(dest("docs"));
+}
+
+function versionWebp() {
+  const opciones = {
+    quality: 50,
+  };
+  return src("assets/**/*.{png,jpg}", { encoding: false })
+    .pipe(newer({ dest: "docs/img", ext: ".webp" }))
+    .pipe(webp(opciones))
+    .pipe(dest("docs/img", { encoding: false }));
+}
+
+function versionAvif() {
+  const opciones = {
+    quality: 50,
+  };
+  return src("assets/**/*.{png,jpg}", { encoding: false })
+    .pipe(newer({ dest: "docs/img", ext: ".avif" }))
+    .pipe(avif(opciones))
+    .pipe(dest("docs/img", { encoding: false }));
+}
+
+function dev() {
+  watch("src/scss/**/*.scss", css);
+  watch("assets/**/*", imagenes);
+  watch("src/js/**/*.js", javascript);
+  watch("index.html", html);
+}
+
+exports.css = css;
+exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.javascript = javascript;
+exports.default = series(
+  imagenes,
+  versionWebp,
+  versionAvif,
+  css,
+  javascript,
+  html,
+  dev
+);
